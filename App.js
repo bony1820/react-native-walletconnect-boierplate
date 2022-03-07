cee5bf529150060bf8b5288814c9a31a964d584f
tuyenbug <85801622+tuyenbug@users.noreply.github.com>
2022-03-07 14:41:16 +0700

    init project

---
+++ App.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useWalletConnect, withWalletConnect } from '@walletconnect/react-native-dapp';
+import * as React from 'react';
+import { Button } from 'react-native'
+
+function App() {
+  const connector = useWalletConnect();
+  if (!connector.connected) {
+    /**
+     *  Connect! 🎉
+     */
+    return <Button title="Connect" onPress={() => connector.connect()} />;
+  }
+  return <Button title="Kill Session" onPress={() => connector.killSession()} />;
+}
+
+export default withWalletConnect(App, {
+  redirectUrl: Platform.OS === 'web' ? window.location.origin : 'yourappscheme://',
+  storageOptions: {
+    asyncStorage: AsyncStorage,
+  },
+});
